1a421b642e0ec80a5dd066a91f175782140cba50
klodian shaba <dev.klodian@gmail.com>
2021-12-17 16:55:14 +0100

    gts init

---
+++ build/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+const welcome = 'world';
+function hello(world = welcome) {
+    return `Hello ${world}! `;
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
